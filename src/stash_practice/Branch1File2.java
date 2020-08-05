a9b049c25cfa6da684ce6eeb3978ab299fd6e797
SuparnaNJune20 <nikam.suparna.s@gmail.com>
2020-08-05 22:01:26 +0530

    Branch1 file 1,2,3 commit

---
+++ src/stash_practice/Branch1File2.java
@@ -0,0 +1,11 @@
+package stash_practice;
+
+public class Branch1File2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In Barnch 1 file 2 sop");
+
+	}
+
+}
---
+++ src/stash_practice/Branch1File3.java
@@ -0,0 +1,10 @@
+package stash_practice;
+
+public class Branch1File3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In Barnch 1 file 3 sop");
+	}
+
+}
