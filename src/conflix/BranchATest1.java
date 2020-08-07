d89884ad936f49111ff295e4bf2fad86b17cdaa2
SuparnaNJune20 <nikam.suparna.s@gmail.com>
2020-08-07 22:47:57 +0530

    Branch A

---
+++ src/conflix/BranchATest1.java
@@ -0,0 +1,10 @@
+package conflix;
+
+public class BranchATest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("In Branch A teat 1 file ");
+	}
+
+}
---
+++ src/p1/Branch2File3.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class Branch2File3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("In branch2 file 3");
+		System.out.println("In branch2 file 3 Modified ");
+	}
+
+}
